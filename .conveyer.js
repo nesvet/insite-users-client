49f13ff5e7d78d95607715021c11520615e6e9b4
Eugene Nesvetaev <dev@nesvet.dev>
2024-08-31 19:28:34 +0400

    chore: @nesvet/conveyer added

---
+++ .conveyer.js
@@ -0,0 +1,27 @@
+import path from "node:path";
+import { Conveyer, ESBuild } from "@nesvet/conveyer";
+
+
+const { NODE_ENV } = process.env;
+
+const distDir = "dist";
+
+
+new Conveyer([
+	
+	new ESBuild({
+		entryPoints: [ "src/index.ts" ],
+		outfile: path.resolve(distDir, "index.js"),
+		external: [ true, "insite-*" ],
+		platform: "neutral",
+		target: "es2020",
+		format: "esm",
+		sourcemap: true,
+		define: {
+			"process.env.NODE_ENV": JSON.stringify(NODE_ENV)
+		}
+	})
+	
+], {
+	initialCleanup: distDir
+});
